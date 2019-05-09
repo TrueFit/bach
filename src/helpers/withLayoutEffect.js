18a2f8e9ef1bca5ef23dcee51203b430d03986ce
Josh Gretz <jgretz@truefit.io>
2019-05-09 15:26:37 -0400

    add with layout, and bump to alpha release

---
+++ src/helpers/withLayoutEffect.js
@@ -0,0 +1,22 @@
+import {REACT, PROPS} from '../util/constants';
+import CONSUMER from '../util/consumer';
+import generateConditionCode from '../util/generateConditionCode';
+
+export default (fn = CONSUMER, conditions) => ({
+  globalDependencies,
+  generateNewVariable,
+}) => {
+  const fnName = generateNewVariable();
+  const conditionCode = generateConditionCode(conditions);
+
+  return {
+    dependencies: {
+      ...globalDependencies,
+      [fnName]: fn,
+    },
+    initialize: `${REACT}.useLayoutEffect(function () {
+        return ${fnName}(${PROPS});
+      }, [${conditionCode}]);`,
+    props: [fnName],
+  };
+};
