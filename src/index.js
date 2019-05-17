72239cb8fd9fc10407c58d3cd739ceba07c5e7cb
Josh Gretz <jgretz@truefit.io>
2019-05-17 16:51:22 -0400

    export generateConditions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export {default as compose} from './compose';
 
 export * from './enhancers';
+
 export * from './util/constants';
+export {default as generateConditionCode} from './util/generateConditionCode';
