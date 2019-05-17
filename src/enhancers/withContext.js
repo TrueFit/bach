c731df0f739afd608ac482824fee7299b19e54df
Josh Gretz <jgretz@truefit.io>
2019-05-17 17:03:02 -0400

    fix react context dependency

--- src/enhancers/withContext.js
+++ src/enhancers/withContext.js
@@ -5,7 +5,9 @@ export default (propertyNames = [], contextName) => () => {
   const contextProps = propertyNames.join(',');
 
   return {
-    dependencies: {REACT: React},
+    dependencies: {
+      [REACT]: React,
+    },
     initialize: `const {${contextProps}} = ${REACT}.useContext(${PROPS}.${contextName});`,
     props: propertyNames,
   };
