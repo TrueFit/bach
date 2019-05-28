f133a53b92f0152571246afd9223f4f3fddaad47
Josh Gretz <jgretz@truefit.io>
2019-05-27 22:01:08 -0400

    update withState

--- src/enhancers/withState.js
+++ src/enhancers/withState.js
@@ -1,35 +1,44 @@
 import {useState, useMemo} from 'react';
 import {PROPS} from '../util/constants';
+import isFunction from '../util/isFunction';
+
+const useObjectAsState = (stateValue, initialValueAlias) => ({
+  dependencies: {},
+  code: `const ${stateValue} = useState(${initialValueAlias});`,
+});
+
+const useFunctionAsState = (stateValue, initialValueAlias) => ({
+  dependencies: {useMemo},
+  code: `
+    const ${stateValue} = useState(
+      useMemo(() => ${initialValueAlias}(${PROPS}), [])
+    );
+  `,
+});
 
 export default (stateName, stateUpdaterName, initialValue) => ({
   generateNewVariable,
 }) => {
-  const initialValueAlias = generateNewVariable();
   const stateValue = generateNewVariable();
-  const initialValueIsFunction = Object.prototype.toString.call(initialValue) === '[object Function]';
-  const enhancerObj = {
+  const initialValueAlias = generateNewVariable();
+
+  const useStateGenerator = isFunction(initialValue)
+    ? useFunctionAsState
+    : useObjectAsState;
+
+  const useStateCode = useStateGenerator(stateValue, initialValueAlias);
+
+  return {
     dependencies: {
       useState,
       [initialValueAlias]: initialValue,
+      ...useStateCode.dependencies,
     },
+    initialize: `
+      ${useStateCode.code}
+      const ${stateName} = ${stateValue}[0];
+      const ${stateUpdaterName} = ${stateValue}[1];
+    `,
     props: [stateName, stateUpdaterName],
   };
-
-  if(initialValueIsFunction) {
-    enhancerObj.dependencies.useMemo = useMemo;
-    const memoizedInitialValueAlias = generateNewVariable();
-    enhancerObj.initialize = `
-      const ${memoizedInitialValueAlias} = useMemo(() => ${initialValueAlias}(${PROPS}), []);
-      const ${stateValue} = useState(${memoizedInitialValueAlias});
-      const ${stateName} = ${stateValue}[0];
-      const ${stateUpdaterName} = ${stateValue}[1];
-    `
-  } else {
-    enhancerObj.initialize = `
-      const ${stateValue} = useState(${initialValueAlias});
-      const ${stateName} = ${stateValue}[0];
-      const ${stateUpdaterName} = ${stateValue}[1];
-    `
-  }
-  return enhancerObj;
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 export {default as compose} from './compose';
 
 export * from './enhancers';
-
 export * from './util/constants';
+
 export {default as generateConditionCode} from './util/generateConditionCode';
 export {default as generateAssignments} from './util/generateAssignments';
+export {default as isFunction} from './util/isFunction';
