7de9f94359c20792d575846550349aa1d0a4c20f
Stephen Jacobs <sjacobs@truefit.io>
2019-06-04 19:58:55 -0400

    updates to Enhancer and compose HOC return types. update tsconfig to include our type def file.

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,27 +2,68 @@
  * Typescript definition file for Bach Compose and Enhancers
  */
 declare module '@truefit/bach' {
-  import {Component, ComponentClass} from 'react';
+  import * as React from 'react';
+  import {ComponentType as Component, StatelessFunctionComponent} from 'react';
 
-  interface ComponentEnhancer<TInner, TOuter> {
-    (component: Component<TInner>): ComponentClass<TOuter>;
+  // Omit taken from https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html
+  type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+  // helper types
+  type mapper<TInner, TOutter> = (input: TInner) => TOutter;
+
+  // compose returns a function that receives a component(inner) and options and returns a
+  // new new wrapped SFC(outer) with all enhancer props passed
+  interface ComposeHOC<TInner, TOuter> {
+    (component: Component<TInner>, options?: {}): StatelessFunctionComponent<
+      TOuter
+    >;
   }
 
+  // Injects props and removes them from the prop requirements.
+  // Optionally, adds new prop requirements from TNeedsProps.
+  // export interface ComponentEnhancerWithProps<
+  //   TInjectedProps,
+  //   TNeedsProps = {}
+  // > {
+  //   <P extends TInjectedProps>(
+  //     component: Component<P>,
+  //   ): React.FunctionComponent<Omit<P, keyof TInjectedProps> & TNeedsProps>;
+  // }
+
+  // Compose pipe
   export function compose<TInner, TOuter>(
     ...enhancers: Function[]
-  ): ComponentEnhancer<TInner, TOuter>;
+  ): ComposeHOC<TInner, TOuter>;
 
-  /**
-   * Enhancers
-   */
+  // enhancers return value
+  type EnhancerReturn<TProps> = {
+    dependencies: object;
+    initialize: string;
+    props: TProps[];
+  };
+
+  interface EnhancerHOC<TProps> {
+    (context: object): EnhancerReturn<TProps>;
+  }
+
+  // withState
+  type stateProps<
+    TState,
+    TStateName extends string,
+    TStateUpdaterName extends string
+  > = {[stateName in TStateName]: TState} &
+    {[stateUpdateName in TStateUpdaterName]: (state: TState) => TState};
+
+  export function withState<
+    TOutter,
+    TState,
+    TStateName extends string,
+    TStateUpdaterName extends string
+  >(
+    stateName: TStateName,
+    stateUpdaterName: TStateUpdaterName,
+    initialState: TState | mapper<TOutter, TState>,
+  ): EnhancerHOC<stateProps<TState, TStateName, TStateUpdaterName>>;
+
   // withEffect
-  // type effectReturn = () => void | void;
-  // type withEffect = (fn: () => effectReturn) => unknown;
-
-  // // withState
-  // type withState = (
-  //   stateName: string,
-  //   stateUpdaterName: string,
-  //   initialState: object | Function,
-  // ) => unknown;
 }
