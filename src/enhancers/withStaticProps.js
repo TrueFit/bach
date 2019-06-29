c7caaa5c1eba90ed46ebf20a554e51b31720c4f5
Josh Gretz <jgretz@truefit.io>
2019-06-28 20:44:55 -0400

    update withStatic props to have a lazy option

--- src/enhancers/withStaticProps.js
+++ src/enhancers/withStaticProps.js
@@ -1,4 +1,29 @@
-export default (props) => () => {
+import {PROPS} from '../util/constants';
+import isFunction from '../util/isFunction';
+
+const lazyStaticProps = (props, generateNewVariable) => {
+  const helperAlias = generateNewVariable();
+  const helper = {};
+
+  return {
+    dependencies: {
+      [helperAlias]: helper,
+    },
+    initialize: `
+      ${helperAlias}.getProps = function() {
+        return ${PROPS};
+      };
+    `,
+    props: [],
+    staticProps: props(helper),
+  };
+};
+
+export default props => ({generateNewVariable}) => {
+  if (isFunction(props)) {
+    return lazyStaticProps(props, generateNewVariable);
+  }
+
   return {
     dependencies: {},
     initialize: '',
