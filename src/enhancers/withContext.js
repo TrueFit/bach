6ddd1140013436fb5dfde222c6ce6c5019ada634
Josh Gretz <jgretz@truefit.io>
2019-08-05 10:55:28 -0500

    update withContext to use alias for contextSource

--- src/enhancers/withContext.js
+++ src/enhancers/withContext.js
@@ -2,20 +2,24 @@ import {useContext} from 'react';
 import {PROPS} from '../util/constants';
 import generateAssignments from '../util/generateAssignments.js';
 
-export default (propertyNames = [], contextSource) => ({generateNewVariable}) => {
+export default (propertyNames = [], contextSource) => ({
+  generateNewVariable,
+}) => {
   const source = generateNewVariable();
   const context = generateNewVariable();
+  const contextSourceAlias = generateNewVariable();
+
   const assignments = generateAssignments(propertyNames, context);
 
   return {
     dependencies: {
       useContext,
-      contextSource,
+      contextSourceAlias: contextSource,
     },
     initialize: `
-      const ${source} = typeof contextSource === 'object' ? contextSource : ${PROPS}.${contextSource};
+      const ${source} = typeof ${contextSourceAlias} === 'object' ? ${contextSourceAlias} : ${PROPS}.${contextSource};
       const ${context} = useContext(${source});
-      
+
       ${assignments}
     `,
     props: propertyNames,
